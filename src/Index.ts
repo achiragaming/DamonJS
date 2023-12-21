4df865bfeecdd66cc7407c775bc6efd4233f177a
Achira Gaming <86994940+achiragaming@users.noreply.github.com>
2023-12-22 01:21:26 +0530

    bug fix

--- src/Index.ts
+++ src/Index.ts
@@ -9,5 +9,5 @@ export * from './DamonJs';
 export { DamonJsTrack, DamonJsQueue, DamonJsPlayer, Plugins };
 export * from './Modules/Interfaces';
 
-export const version = '1.3.6';
+export const version = '1.3.7';
  
