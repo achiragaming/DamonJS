3aba53662bf29947d671b8822f4a72a1485498d9
Achira Gaming <86994940+achiragaming@users.noreply.github.com>
2023-12-27 17:19:18 +0530

    bug fix on destroy not emitting player end

--- src/Managers/DamonJsPlayer.ts
+++ src/Managers/DamonJsPlayer.ts
@@ -128,14 +128,12 @@ export class DamonJsPlayer {
     });
 
     this.player.on('end', (data) => {
-      if (this.state === PlayerState.DESTROYING || this.state === PlayerState.DESTROYED)
-        return this.emit(Events.Debug, this, `Player ${this.guildId} destroyed from end event`);
-
       this.isTrackPlaying = false;
-
       const playedTrack = this.queue[this.queue.playedTrackId];
       if (playedTrack) this.emit(Events.PlayerEnd, this, playedTrack);
 
+      if (this.state === PlayerState.DESTROYING || this.state === PlayerState.DESTROYED)
+        return this.emit(Events.Debug, this, `Player ${this.guildId} destroyed from end event`);
       if (data.reason === 'replaced') return this.emit(Events.PlayerEmpty, this);
 
       if (this.loop === LoopState.Track) {
