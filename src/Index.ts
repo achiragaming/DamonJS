dc0010aa32810d229a1166469f4f086947722df3
Achira Gaming <86994940+achiragaming@users.noreply.github.com>
2024-11-19 23:34:44 +0530

    up

--- src/Index.ts
+++ src/Index.ts
@@ -3,9 +3,9 @@ import { DamonJsTrack } from './Managers/Supports/DamonJsTrack';
 import { DamonJsQueue } from './Managers/Supports/DamonJsQueue';
 import { DamonJsPlayer } from './Managers/DamonJsPlayer';
 import Plugins from './Modules/Plugins';
-
+import d from '../package.json';
 export * from './DamonJs';
 export { DamonJsTrack, DamonJsQueue, DamonJsPlayer, Plugins };
 export * from './Modules/Interfaces';
 
-export const version = '1.9.7';
+export const version = d.version;
